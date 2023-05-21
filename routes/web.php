190b6ce4105aef7a6667f8149e23f86bddb43f2d
yuita-yoshihiko <soup998211@gmail.com>
2023-05-21 10:42:22 +0900

    todoアプリ作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list', [TodoListController::class, 'index']);
+Route::resource('tasks', TaskController::class);
